102c93e52cf6d6802b0f8193b4f0111c910640fc
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-04-06 13:00:47 +0100

    Update test_surv_cvcoxboost.R
    [skip ci]

--- tests/testthat/test_surv_cvcoxboost.R
+++ tests/testthat/test_surv_cvcoxboost.R
@@ -1,8 +1,17 @@
 context("surv.cvcoxboost")
 
 test_that("autotest", {
-  learner = LearnerSurvCVCoxboost$new()
+  set.seed(1)
+  learner = lrn("surv.cvcoxboost", maxstepno = 1, K = 2)
   expect_learner(learner)
-  result = run_autotest(learner)
+  result = run_autotest(learner, N = 10)
   expect_true(result, info = result$error)
+})
+
+test_that("optim",{
+  set.seed(1)
+  learner = lrn("surv.cvcoxboost", penalty = "optimCoxBoostPenalty",
+                maxstepno = 1, minstepno = 0, iter.max = 1, K = 2)
+  task = tgen("simsurv")$generate(10)
+  expect_prediction_surv(suppressWarnings(learner$train(task)$predict(task)))
 })
